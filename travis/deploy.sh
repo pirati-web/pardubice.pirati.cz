412d2d6a140c4613fa690df1e4e687c85257b21a
xaralis <filip.varecha@fragaria.cz>
2018-09-16 14:42:45 +0200

    [fix] travis build

---
+++ travis/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    # Initialize deploy ssh key
+    openssl aes-256-cbc -K $encrypted_dc982276292c_key -iv $encrypted_dc982276292c_iv -in deploy_rsa_sandbox.enc -out /tmp/deploy_rsa_sandbox -d
+    eval "$(ssh-agent -s)"
+    chmod 600 /tmp/deploy_rsa_sandbox
+    ssh-add /tmp/deploy_rsa_sandbox
+
+    # Sync
+    if [ "$0" == "sandbox" ]; then
+        rsync -r --delete-after --quiet -e 'ssh -p 2222' $TRAVIS_BUILD_DIR/_site/ piratipardubice@93.185.103.119:/srv/pirati-tmp
+    if
+
+    if [ "$0" == "production" ]; then
+        rsync -r --delete-after --quiet -e 'ssh -p 2222' $TRAVIS_BUILD_DIR/_site/ piratipardubice@93.185.103.119:/srv/pirati
+    if
+fi
