b940970466ba98c90d5da2111ef4c63404917a95
xaralis <filip.varecha@fragaria.cz>
2018-10-27 12:48:05 +0200

    [feature] fixes travis deployments

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -2,10 +2,10 @@
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     # Initialize deploy ssh key
-    openssl aes-256-cbc -K $encrypted_dc982276292c_key -iv $encrypted_dc982276292c_iv -in deploy_rsa_sandbox.enc -out /tmp/deploy_rsa_sandbox -d
-    openssl aes-256-cbc -K $encrypted_dc982276292c_key -iv $encrypted_dc982276292c_iv -in deploy_rsa_digitalocean.enc -out /tmp/deploy_rsa_digitalocean -d
+    openssl aes-256-cbc -K $encrypted_dc982276292c_key -iv $encrypted_dc982276292c_iv -in deploykeys.tar.enc -out /tmp/deploykeys.tar -d
+    tar xvf /tmp/deploykeys.tar -C /tmp
     eval "$(ssh-agent -s)"
-    chmod 600 /tmp/deploy_rsa_sandbox
+    chmod 600 /tmp/deploy_rsa_xaralis
     chmod 600 /tmp/deploy_rsa_digitalocean
 
     # Sync
@@ -17,7 +17,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     fi
 
     if [ $1 == "production" ]; then
-        ssh-add /tmp/deploy_rsa_sandbox
+        ssh-add /tmp/deploy_rsa_xaralis
         rsync -r --delete-after --quiet -e 'ssh -p 2222' $TRAVIS_BUILD_DIR/_site/ piratipardubice@93.185.103.119:/srv/pirati
     fi
 fi
